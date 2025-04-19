d867b9205f091f0a60d83f39a516311afbe72013
alexago <alexago1@yandex.ru>
2025-04-19 21:15:18 +0000

    Remove unused api_root view from views.py

--- octofit_tracker/views.py
+++ octofit_tracker/views.py
@@ -1,18 +1,6 @@
 from rest_framework import viewsets
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .models import User, Team, Activity, Leaderboard, Workout
 from .serializers import UserSerializer, TeamSerializer, ActivitySerializer, LeaderboardSerializer, WorkoutSerializer
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': 'api/users/',
-        'teams': 'api/teams/',
-        'activities': 'api/activities/',
-        'leaderboard': 'api/leaderboard/',
-        'workouts': 'api/workouts/',
-    })
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
